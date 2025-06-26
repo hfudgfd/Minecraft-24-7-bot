e03ac999b06f4ea6428352b45d6738c2fc824b90
john <williamlemus887@gmail.com>
2025-06-25 22:13:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const mineflayer = require('mineflayer');
 
 // Define the bot's username and password
-const botUsername = 'Username'; // Your bot's username
-const password = 'password';       // Your bot's password
+const botUsername = 'AFKbot'; // Your bot's username
+const password = 'NIGGER';       // Your bot's password
 
 let bot = mineflayer.createBot({
-  host: 'SERVER IP', // Server IP
-  port: 19132,                    // Server port
+  host: 'PLUMSMP.aternos.me', // Server IP
+  port: 12862,                    // Server port
   username: botUsername,          // Bot's name (do not change here)
   version: '1.16.4'               // Server version
 });
